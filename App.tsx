efa81cd8b01c9f7d26625b23c4b889e4bf8dccb0
HuyHoang <dragonhuyhoang2001@gmail.com>
2023-01-05 14:41:14 +0700

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Image, SectionList, StyleSheet, Text, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'gray',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+const SectionListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <SectionList
+        sections={[
+          {title: 'D', data: ['Devin', 'Dan', 'Dominic']},
+          {
+            title: 'J',
+            data: [
+              'Jackson',
+              'James',
+              'Jillian',
+              'Jimmy',
+              'Joel',
+              'John',
+              'Julie',
+            ],
+          },
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+        renderSectionHeader={({section}) => (
+          <Text style={styles.sectionHeader}>{section.title}</Text>
+        )}
+        keyExtractor={(_, index) => `basicListEntry-${index}}`}
+      />
+      <Image
+        source={{uri: 'https://reactjs.org/logo-og.png'}}
+        style={{width: 200, height: 200}}
+      />
+    </View>
+  );
+};
+
+export default SectionListBasics;
